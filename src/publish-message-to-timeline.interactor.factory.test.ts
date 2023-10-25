77b8c6019c8fd399ddc0d9b5c95c45cd2cf11166
Yeiniel Suarez Sosa <1140833+yeiniel@users.noreply.github.com>
2023-10-24 23:36:19 -0500

    define publishMessageToTimelineInteractorFactory

--- src/publish-message-to-timeline.interactor.factory.test.ts
+++ src/publish-message-to-timeline.interactor.factory.test.ts
@@ -1,5 +1,7 @@
 import { describe, expect, it } from "@jest/globals";
 
+function publishMessageToTimelineInteractorFactory() {}
+
 describe(publishMessageToTimelineInteractorFactory.name, () => {
     it("should be defined", () => {
         expect(publishMessageToTimelineInteractorFactory).toBeDefined();
