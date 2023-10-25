40e540e194b7b74cae8a3ab92e01848823010f14
Yeiniel Suarez Sosa <1140833+yeiniel@users.noreply.github.com>
2023-10-24 23:55:10 -0500

    check that publishMessageToTimelineInteractorFactory product should return the result of calling store(author, message)

--- src/publish-message-to-timeline.interactor.factory.test.ts
+++ src/publish-message-to-timeline.interactor.factory.test.ts
@@ -1,7 +1,10 @@
-import { afterEach, describe, expect, it, jest } from "@jest/globals";
+import { beforeEach, describe, expect, it, jest } from "@jest/globals";
 
+type User = unknown;
+type Message = unknown;
 /** Store Message into User Timeline */
-type StoreFn = (...args: unknown[]) => Promise<unknown>;
+type StoreFn = (author: User, message: Message) => Promise<unknown>;
+type PublishMessageToTimelineInteractor = (author: User, message: Message) => Promise<unknown>;
 
 function publishMessageToTimelineInteractorFactory(_store: StoreFn) {
     return function publishMessageToTimelineInteractor() {}
@@ -14,5 +17,26 @@ describe(publishMessageToTimelineInteractorFactory.name, () => {
         const interactor = publishMessageToTimelineInteractorFactory(store);
 
         expect(typeof interactor).toBe("function");
+    });
+
+    describe("product", () => {
+        let store: jest.Mock<StoreFn>;
+        let interactor: PublishMessageToTimelineInteractor;
+
+        beforeEach(() => {
+            store = jest.fn<StoreFn>();
+            interactor = publishMessageToTimelineInteractorFactory(store);
+        });
+
+        it("should return the result of calling store(author, message)", async () => {
+            const author = 'Alice';
+            const message = 'Hello World!';
+
+            const result = await interactor(author, message);
+
+            expect(store).toBeCalledTimes(1);
+            expect(store).toBeCalledWith(author, message);
+            expect(store).toReturnWith(result);
+        });
     });
 });
