97a65d492c86d9b29c7162e5fdd8f0f142faab59
Yeiniel Suarez Sosa <1140833+yeiniel@users.noreply.github.com>
2023-10-25 00:04:13 -0500

    define inMemoryStoreFactory

--- src/in-memory-store.factory.test.ts
+++ src/in-memory-store.factory.test.ts
@@ -1,5 +1,7 @@
 import { describe, expect, it } from "@jest/globals";
 
+function inMemoryStoreFactory() {}
+
 describe(inMemoryStoreFactory.name, () => {
     it("should be defined", () => {
         expect(inMemoryStoreFactory).toBeDefined();
