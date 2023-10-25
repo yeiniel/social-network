dd7bf753dce526d8f5148459bda58c11e8023978
Yeiniel Suarez Sosa <1140833+yeiniel@users.noreply.github.com>
2023-10-24 23:42:05 -0500

    check that publishMessageToTimelineInteractorFactory should return callable when called with store

--- src/publish-message-to-timeline.interactor.factory.test.ts
+++ src/publish-message-to-timeline.interactor.factory.test.ts
@@ -1,9 +1,20 @@
-import { describe, expect, it } from "@jest/globals";
+import { afterEach, describe, expect, it, jest } from "@jest/globals";
+
+/** Store Message into User Timeline */
+type StoreFn = (...args: unknown[]) => Promise<unknown>;
 
 function publishMessageToTimelineInteractorFactory() {}
 
 describe(publishMessageToTimelineInteractorFactory.name, () => {
     it("should be defined", () => {
         expect(publishMessageToTimelineInteractorFactory).toBeDefined();
+    });
+
+    it("should return callable when called with store", () => {
+        const store = jest.fn<StoreFn>();
+
+        const interactor = publishMessageToTimelineInteractorFactory(store);
+
+        expect(typeof interactor).toBe("function");
     });
 });
