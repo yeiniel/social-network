bdf58e6a00c4f509281132bef24231380037a5fd
Yeiniel Suarez Sosa <1140833+yeiniel@users.noreply.github.com>
2023-10-25 20:05:20 -0500

    check that applicationFactory should return a new instance of the express application

--- src/application.factory.test.ts
+++ src/application.factory.test.ts
@@ -1,9 +1,20 @@
-import { describe, expect, it } from "@jest/globals";
+import { describe, expect, it, jest } from "@jest/globals";
+jest.mock("express");
+import express from "express";
 
 function applicationFactory() {}
 
 describe(applicationFactory.name, () => {
     it("should be defined", () => {
         expect(applicationFactory).toBeDefined();
+    });
+
+    it("should return a new instance of the express application", () => {
+        jest.mocked(express).mockImplementation(jest.requireActual("express"));
+
+        const app = applicationFactory();
+
+        expect(express).toBeCalledTimes(1);
+        expect(app).toBe(jest.mocked(express).mock.results[0]!.value);
     });
 });
