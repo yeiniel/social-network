f0359c122bf3f28758535e9c5abaa503a80ae29c
Yeiniel Suarez Sosa <1140833+yeiniel@users.noreply.github.com>
2023-10-25 19:51:56 -0500

    define applicationFactory

--- src/application.factory.test.ts
+++ src/application.factory.test.ts
@@ -1,5 +1,7 @@
 import { describe, expect, it } from "@jest/globals";
 
+function applicationFactory() {}
+
 describe(applicationFactory.name, () => {
     it("should be defined", () => {
         expect(applicationFactory).toBeDefined();
