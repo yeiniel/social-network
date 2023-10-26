8b36e19f31e453b98ee6c87b77e403113de31ddb
Yeiniel Suarez Sosa <1140833+yeiniel@users.noreply.github.com>
2023-10-25 21:41:26 -0500

    refactor existing test to make it less dependent on the structure of applicationFactory

--- src/application.factory.test.ts
+++ src/application.factory.test.ts
@@ -2,7 +2,7 @@ import { afterEach, beforeEach, describe, expect, it, jest } from "@jest/globals
 jest.mock("express");
 jest.mock("express-openid-connect");
 import request from "supertest";
-import express, { Application, NextFunction } from "express";
+import express, { NextFunction } from "express";
 import { auth } from "express-openid-connect";
 
 class MessageRequiredError extends Error {
@@ -36,29 +36,30 @@ describe(applicationFactory.name, () => {
     
     afterEach(() => {
         jest.resetAllMocks();
-        jest.restoreAllMocks();
-    });
-
-    it("should use the express-openid-connect auth middleware", () => {
-        const actualApp = jest.requireActual<typeof express>("express")();
-         jest.spyOn(actualApp, "use");
-         jest.mocked<() => Application>(express).mockImplementation(() => actualApp);
-
-         const app = applicationFactory();
-
-         expect(auth).toBeCalledTimes(1);
-         expect(jest.mocked(auth).mock.calls[0]!.length).toBe(0);
-         expect(app.use).toBeCalledWith(jest.mocked(auth).mock.results[0]!.value);
     });
 
     describe("product", () => {
         describe("while publishing message to personal timeline", () => {
             const endpoint = "/compose";
+
+            function requestFactory() {
+                return request(applicationFactory())
+                    .post(endpoint)
+                    .set('Content-Type', 'application/x-www-form-urlencoded');
+            }
+
+            it("should authenticate user", async () => {
+                const message = 'User not authenticated';
+                jest.mocked(auth).mockReturnValue((_req: unknown, _res: unknown, next: NextFunction) => next(new Error(message)));
+   
+                const res = await requestFactory();
+   
+                expect(res.status).toBe(500);
+                expect(res.text).toContain(message);
+            });
     
             it("should validate message", async () => {
-                const res = await request(applicationFactory())
-                     .post(endpoint)
-                     .set('Content-Type', 'application/x-www-form-urlencoded');
+                const res = await requestFactory();
     
                  expect(res.status).toBe(500);
                  expect(res.text).toContain(new MessageRequiredError().message);
