23a0ef73423cb9c11f983af32c3e7ef9365c7461
Yeiniel Suarez Sosa <1140833+yeiniel@users.noreply.github.com>
2023-10-25 21:27:09 -0500

    check that applicationFactory product while publishing message to personal timeline should validate message

--- src/application.factory.test.ts
+++ src/application.factory.test.ts
@@ -1,8 +1,15 @@
 import { afterEach, beforeEach, describe, expect, it, jest } from "@jest/globals";
 jest.mock("express");
 jest.mock("express-openid-connect");
+import request from "supertest";
 import express, { Application, NextFunction } from "express";
 import { auth } from "express-openid-connect";
+
+class MessageRequiredError extends Error {
+    constructor() {
+        super("Message is required");
+    }
+}
 
 function applicationFactory(authMiddlewareFactory = auth, expressFactory = express) {
     const app = expressFactory();
@@ -14,6 +21,7 @@ function applicationFactory(authMiddlewareFactory = auth, expressFactory = expre
 
 describe(applicationFactory.name, () => {
     beforeEach(() => {
+        jest.mocked(express).mockImplementation(jest.requireActual("express"));
         jest.mocked(auth).mockReturnValue((_req: unknown, _res: unknown, next: NextFunction) => next());
     });
     
@@ -31,7 +39,21 @@ describe(applicationFactory.name, () => {
 
          expect(auth).toBeCalledTimes(1);
          expect(jest.mocked(auth).mock.calls[0]!.length).toBe(0);
-         expect(app.use).toBeCalledTimes(1);
          expect(app.use).toBeCalledWith(jest.mocked(auth).mock.results[0]!.value);
+    });
+
+    describe("product", () => {
+        describe("while publishing message to personal timeline", () => {
+            const endpoint = "/compose";
+    
+            it("should validate message", async () => {
+                const res = await request(applicationFactory())
+                     .post(endpoint)
+                     .set('Content-Type', 'application/x-www-form-urlencoded');
+    
+                 expect(res.status).toBe(500);
+                 expect(res.text).toContain(new MessageRequiredError().message);
+            });
+        });
     });
 });
