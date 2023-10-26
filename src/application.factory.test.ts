5f89bbc3f3e61e493ddb9d05e9f1b2f47cfeb31c
Yeiniel Suarez Sosa <1140833+yeiniel@users.noreply.github.com>
2023-10-25 21:09:48 -0500

    check that applicationFactory should use the express-openid-connect auth middleware

--- src/application.factory.test.ts
+++ src/application.factory.test.ts
@@ -1,12 +1,23 @@
-import { describe, expect, it, jest } from "@jest/globals";
+import { afterEach, beforeEach, describe, expect, it, jest } from "@jest/globals";
 jest.mock("express");
-import express from "express";
+jest.mock("express-openid-connect");
+import express, { Application, NextFunction } from "express";
+import { auth } from "express-openid-connect";
 
 function applicationFactory(expressFactory = express) {
     return expressFactory();
 }
 
 describe(applicationFactory.name, () => {
+    beforeEach(() => {
+        jest.mocked(auth).mockReturnValue((_req: unknown, _res: unknown, next: NextFunction) => next());
+    });
+    
+    afterEach(() => {
+        jest.resetAllMocks();
+        jest.restoreAllMocks();
+    });
+
     it("should return a new instance of the express application", () => {
         jest.mocked(express).mockImplementation(jest.requireActual("express"));
 
@@ -14,5 +25,18 @@ describe(applicationFactory.name, () => {
 
         expect(express).toBeCalledTimes(1);
         expect(app).toBe(jest.mocked(express).mock.results[0]!.value);
+    });
+
+    it("should use the express-openid-connect auth middleware", () => {
+        const actualApp = jest.requireActual<typeof express>("express")();
+         jest.spyOn(actualApp, "use");
+         jest.mocked<() => Application>(express).mockImplementation(() => actualApp);
+
+         const app = applicationFactory();
+
+         expect(auth).toBeCalledTimes(1);
+         expect(jest.mocked(auth).mock.calls[0]!.length).toBe(0);
+         expect(app.use).toBeCalledTimes(1);
+         expect(app.use).toBeCalledWith(jest.mocked(auth).mock.results[0]!.value);
     });
 });
