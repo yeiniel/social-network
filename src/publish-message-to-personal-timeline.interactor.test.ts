48806ec4062d29b9cd8870ef19e3d9683e594e16
Yeiniel Suarez Sosa <1140833+yeiniel@users.noreply.github.com>
2024-02-01 21:40:49 -0500

    define PublishMessageToPersonalTimelineInteractor

--- src/publish-message-to-personal-timeline.interactor.test.ts
+++ src/publish-message-to-personal-timeline.interactor.test.ts
@@ -1,5 +1,7 @@
 import { describe, it, expect } from '@jest/globals';
 
+class PublishMessageToPersonalTimelineInteractor {}
+
 describe(PublishMessageToPersonalTimelineInteractor.name, () => {
     it('should be defined', () => {
         expect(PublishMessageToPersonalTimelineInteractor).toBeDefined();
