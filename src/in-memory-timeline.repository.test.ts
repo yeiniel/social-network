6c4b33cf7f122d3882448ab7e80b5620c7335dde
Yeiniel Suarez Sosa <1140833+yeiniel@users.noreply.github.com>
2024-02-01 22:28:19 -0500

    assert InMemoryTimelineRepository.prototype.store should set map entry with key owner to array of first message

--- src/in-memory-timeline.repository.test.ts
+++ src/in-memory-timeline.repository.test.ts
@@ -1,5 +1,7 @@
 import { describe, expect, it } from "@jest/globals";
 import { TimelineRepository } from "./timeline.repository.js";
+import { User } from "./user.js";
+import { Message } from "./message.js";
 
 class InMemoryTimelineRepository implements TimelineRepository {
     public async store(): Promise<void> {}
@@ -9,6 +11,17 @@ describe(InMemoryTimelineRepository.name, () => {
     describe(InMemoryTimelineRepository.prototype.store.name, () => {
         it('should be defined', () => {
             expect(InMemoryTimelineRepository.prototype.store).toBeDefined();
+        });
+
+        it('should set map entry with key owner to array of first message', async () => {
+            const owner: User = 'some-random-author';
+            const message: Message = 'some-random-message'; 
+            const map = new Map<User, Message[]>();
+            const timelineRepository = new InMemoryTimelineRepository(map);
+
+            await timelineRepository.store(owner, message);
+
+            expect(map.get(owner)).toEqual([message]);
         });
     })
 });
