9c747b0219129ec8be739835b8b3ab194a8c2501
Yeiniel Suarez Sosa <1140833+yeiniel@users.noreply.github.com>
2024-02-01 22:01:53 -0500

    update PublishMessageToPersonalTimelineInteractor.prototype.execute call timelineRepository.store with author and message

--- src/publish-message-to-personal-timeline.interactor.test.ts
+++ src/publish-message-to-personal-timeline.interactor.test.ts
@@ -5,7 +5,11 @@ type Message = unknown;
 type TimelineRepository = { store: (author: User, message: Message) => Promise<unknown>; };
 
 class PublishMessageToPersonalTimelineInteractor {
-    public execute() {}
+    constructor(private readonly timelineRepository?: TimelineRepository) { }
+
+    public async execute(author: User, message: Message) {
+        await this.timelineRepository?.store(author, message);
+    }
 }
 
 describe(PublishMessageToPersonalTimelineInteractor.name, () => {
