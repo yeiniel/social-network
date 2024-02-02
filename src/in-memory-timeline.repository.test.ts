864ded597fd42cd8e02c04d05e76ae9a011fee59
Yeiniel Suarez Sosa <1140833+yeiniel@users.noreply.github.com>
2024-02-01 22:14:16 -0500

    define InMemoryTimelineRepository

--- src/in-memory-timeline.repository.test.ts
+++ src/in-memory-timeline.repository.test.ts
@@ -1,5 +1,7 @@
 import { describe, expect, it } from "@jest/globals";
 
+class InMemoryTimelineRepository {}
+
 describe(InMemoryTimelineRepository.name, () => {
     it('should be defined', () => {
         expect(InMemoryTimelineRepository).toBeDefined();
