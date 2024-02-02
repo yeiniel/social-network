f7d8a2cdeed855e47f0818ff98ba0cfd0fed22db
Yeiniel Suarez Sosa <1140833+yeiniel@users.noreply.github.com>
2024-02-01 21:47:11 -0500

    assert PublishMessageToPersonalTimelineInteractor.prototype.execute should be of type function

--- src/publish-message-to-personal-timeline.interactor.test.ts
+++ src/publish-message-to-personal-timeline.interactor.test.ts
@@ -6,4 +6,10 @@ describe(PublishMessageToPersonalTimelineInteractor.name, () => {
     it('should be defined', () => {
         expect(PublishMessageToPersonalTimelineInteractor).toBeDefined();
     });
+
+    describe(PublishMessageToPersonalTimelineInteractor.prototype.execute.name, () => {
+        it('should be of type function', () => {
+            expect(typeof (new PublishMessageToPersonalTimelineInteractor()).execute).toBe('function');
+        });
+    });
 });
