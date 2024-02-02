3ff81f006f2a21209541ef2b70e644620f17f8b6
Yeiniel Suarez Sosa <1140833+yeiniel@users.noreply.github.com>
2024-02-01 21:49:05 -0500

    refactor PublishMessageToPersonalTimelineInteractor test suite
    remove not needed test

--- src/publish-message-to-personal-timeline.interactor.test.ts
+++ src/publish-message-to-personal-timeline.interactor.test.ts
@@ -5,10 +5,6 @@ class PublishMessageToPersonalTimelineInteractor {
 }
 
 describe(PublishMessageToPersonalTimelineInteractor.name, () => {
-    it('should be defined', () => {
-        expect(PublishMessageToPersonalTimelineInteractor).toBeDefined();
-    });
-
     describe(PublishMessageToPersonalTimelineInteractor.prototype.execute.name, () => {
         it('should be of type function', () => {
             expect(typeof (new PublishMessageToPersonalTimelineInteractor()).execute).toBe('function');
