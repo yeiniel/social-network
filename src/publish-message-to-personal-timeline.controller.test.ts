7269b1b762c1941fba84e65b24ba06d60ba4141c
Yeiniel Suarez Sosa <1140833+yeiniel@users.noreply.github.com>
2024-02-03 13:58:34 -0500

    assert PublishMessageToPersonalTimelineController should be defined

---
+++ src/publish-message-to-personal-timeline.controller.test.ts
@@ -0,0 +1,7 @@
+import { describe, expect, it } from '@jest/globals';
+
+describe(PublishMessageToPersonalTimelineController.name, () => {
+    it('should be defined', () => {
+        expect(PublishMessageToPersonalTimelineController).toBeDefined();
+    });
+});
