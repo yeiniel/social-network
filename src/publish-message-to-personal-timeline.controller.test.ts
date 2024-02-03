37e18992b4a21f84ae5285087485949fdbabfb1d
Yeiniel Suarez Sosa <1140833+yeiniel@users.noreply.github.com>
2024-02-03 14:03:41 -0500

    refactor PublishMessageToPersonalTimelineController test suite
    remove not needed test

--- src/publish-message-to-personal-timeline.controller.test.ts
+++ src/publish-message-to-personal-timeline.controller.test.ts
@@ -3,10 +3,6 @@ import { describe, expect, it } from '@jest/globals';
 class PublishMessageToPersonalTimelineController {}
 
 describe(PublishMessageToPersonalTimelineController.name, () => {
-    it('should be defined', () => {
-        expect(PublishMessageToPersonalTimelineController).toBeDefined();
-    });
-
     describe(PublishMessageToPersonalTimelineController.prototype.handle.name, () => {
         it('should be defined', () => {
             expect((new PublishMessageToPersonalTimelineController()).handle).toBeDefined();
