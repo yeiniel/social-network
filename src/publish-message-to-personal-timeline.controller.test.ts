5b86f65f16e95c393a5ac6ad55bd0d04cc15fb59
Yeiniel Suarez Sosa <1140833+yeiniel@users.noreply.github.com>
2024-02-03 14:01:01 -0500

    define class PublishMessageToPersonalTimelineController

--- src/publish-message-to-personal-timeline.controller.test.ts
+++ src/publish-message-to-personal-timeline.controller.test.ts
@@ -1,5 +1,7 @@
 import { describe, expect, it } from '@jest/globals';
 
+class PublishMessageToPersonalTimelineController {}
+
 describe(PublishMessageToPersonalTimelineController.name, () => {
     it('should be defined', () => {
         expect(PublishMessageToPersonalTimelineController).toBeDefined();
