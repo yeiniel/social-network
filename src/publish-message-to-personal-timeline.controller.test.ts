6301e2379fd96cae984d20f82041fdb77cc1fc3f
Yeiniel Suarez Sosa <1140833+yeiniel@users.noreply.github.com>
2024-02-03 14:03:16 -0500

    assert PublishMessageToPersonalTimelineController.prototype.handle should be defined

--- src/publish-message-to-personal-timeline.controller.test.ts
+++ src/publish-message-to-personal-timeline.controller.test.ts
@@ -6,4 +6,10 @@ describe(PublishMessageToPersonalTimelineController.name, () => {
     it('should be defined', () => {
         expect(PublishMessageToPersonalTimelineController).toBeDefined();
     });
+
+    describe(PublishMessageToPersonalTimelineController.prototype.handle.name, () => {
+        it('should be defined', () => {
+            expect((new PublishMessageToPersonalTimelineController()).handle).toBeDefined();
+        });
+    });
 });
