8625cb264738312a7878636fc1120a03259199a6
Yeiniel Suarez Sosa <1140833+yeiniel@users.noreply.github.com>
2024-02-03 22:05:11 -0500

    assert PublishMessageToPersonalTimelineController.prototype.handle should set response status code to 500 if no message provided

--- src/publish-message-to-personal-timeline.controller.test.ts
+++ src/publish-message-to-personal-timeline.controller.test.ts
@@ -1,6 +1,8 @@
 import { describe, expect, it } from '@jest/globals';
 import request from 'supertest';
 import express, { Request, Response, NextFunction } from 'express';
+import { randomUserFactory } from './testing/random-user.factory.js';
+import { User } from './user.js';
 
 class PublishMessageToPersonalTimelineController {
     handle(req: Request, res: Response, next: NextFunction) {
@@ -23,6 +25,23 @@ describe(PublishMessageToPersonalTimelineController.name, () => {
             const res = await request(app).post(endpoint);
 
             expect(res.status).toBe(401);
+        });
+
+        it('should set response status code to 500 if no message provided', async () => {
+            const endpoint = '/api/v1/personal-timeline';
+            const controller = new PublishMessageToPersonalTimelineController();
+
+            const app = express();
+            app.use((req, _, next) => {
+                (req as unknown as { user: User }).user = randomUserFactory();
+                next();
+            });
+            app.post(endpoint, controller.handle);
+
+            const res = await request(app).post(endpoint);
+
+            expect(res.status).toBe(500);
+            expect(res.text).toContain('Message is missing');
         });
     });
 });
