9030f4ccfadbd4093c6b2d368963c45835442036
Yeiniel Suarez Sosa <1140833+yeiniel@users.noreply.github.com>
2024-02-03 21:46:08 -0500

    assert PublishMessageToPersonalTimelineController.prototype.handle should set response status code to 401 if no author provided

--- src/publish-message-to-personal-timeline.controller.test.ts
+++ src/publish-message-to-personal-timeline.controller.test.ts
@@ -1,4 +1,6 @@
 import { describe, expect, it } from '@jest/globals';
+import request from 'supertest';
+import express from 'express';
 
 class PublishMessageToPersonalTimelineController {
     handle() {}
@@ -8,6 +10,18 @@ describe(PublishMessageToPersonalTimelineController.name, () => {
     describe(PublishMessageToPersonalTimelineController.prototype.handle.name, () => {
         it('should be defined', () => {
             expect((new PublishMessageToPersonalTimelineController()).handle).toBeDefined();
+        });
+
+        it('should set response status code to 401 if no author provided', async () => {
+            const endpoint = '/api/v1/personal-timeline';
+            const controller = new PublishMessageToPersonalTimelineController();
+
+            const app = express();
+            app.post(endpoint, controller.handle);
+
+            const res = await request(app).post(endpoint);
+
+            expect(res.status).toBe(401);
         });
     });
 });
